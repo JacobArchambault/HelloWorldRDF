a7179ba01056a757e6ab482857fcd99b4cfe6cd1
Jacob Archambault <jacob.archambault@waystar.com>
2021-02-03 15:51:17 -0500

    Inline "Hello World" and "Bonjour tout le Monde" nodes.

--- Program.cs
+++ Program.cs
@@ -13,11 +13,9 @@ namespace HelloWorldRDF
 
             IUriNode dotNetRDF = g.CreateUriNode(UriFactory.Create("http://www.dotnetrdf.org"));
             IUriNode says = g.CreateUriNode(UriFactory.Create("http://example.org/says"));
-            ILiteralNode helloWorld = g.CreateLiteralNode("Hello World");
-            ILiteralNode bonjourMonde = g.CreateLiteralNode("Bonjour tout le Monde", "fr");
 
-            g.Assert(new Triple(dotNetRDF, says, helloWorld));
-            g.Assert(new Triple(dotNetRDF, says, bonjourMonde));
+            g.Assert(new Triple(dotNetRDF, says, g.CreateLiteralNode("Hello World")));
+            g.Assert(new Triple(dotNetRDF, says, g.CreateLiteralNode("Bonjour tout le Monde", "fr")));
 
             foreach (Triple t in g.Triples)
             {
